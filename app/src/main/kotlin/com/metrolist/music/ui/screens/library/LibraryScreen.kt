40706e9e71817320098b32883017a3f5943bb40e
Fhox006 <fedefhox@gmail.com>
2026-04-27 12:58:40 +0200

    .

--- app/src/main/kotlin/com/metrolist/music/ui/screens/library/LibraryScreen.kt
+++ app/src/main/kotlin/com/metrolist/music/ui/screens/library/LibraryScreen.kt
@@ -6,25 +6,14 @@
 package com.metrolist.music.ui.screens.library
 
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.metrolist.music.R
 import com.metrolist.music.constants.LibraryFilter
 
 enum class LibraryView { LIBRARY, DOWNLOADS }
@@ -38,22 +27,7 @@ fun LibraryScreen(navController: NavController) {
         when {
             selectedCategory == LibraryFilter.PLAYLISTS -> LibraryPlaylistsScreen(
                 navController = navController,
-                filterContent = {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        Spacer(Modifier.width(12.dp))
-                        FilterChip(
-                            selected = true,
-                            onClick = { selectedCategory = null },
-                            label = { Text(stringResource(R.string.playlists)) },
-                            leadingIcon = {
-                                Icon(
-                                    painter = painterResource(R.drawable.close),
-                                    contentDescription = null,
-                                )
-                            },
-                        )
-                    }
-                },
+                onDeselect = { selectedCategory = null },
             )
             selectedCategory == LibraryFilter.SONGS -> LibrarySongsScreen(
                 navController = navController,
