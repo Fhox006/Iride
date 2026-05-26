fdbeb8195c888da03d37d28c01d80a91d668996b
Fhox006 <fedefhox@gmail.com>
2026-05-26 23:10:53 +0200

    release: v0.1.0-alpha06

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         applicationId = applicationIdOverride ?: baseApplicationId
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "0.1.0-alpha06"
         resValue("string", "app_name", appNameOverride ?: "Iride")
 
